5f58cc556d3253c8fe7f424f8b7b2bb4b12282a9
nelfisahra <nelfisahrasahra@gmail.com>
2024-08-27 22:08:28 +0700

    membuat statment dan expresi

---
+++ 4 menulis statement dan ekspresi .php
@@ -0,0 +1,20 @@
+<?php
+echo "ini statement 1";
+echo "ini statement 2";
+//deklarasi sebuah variabel
+$b=15;
+$c=7;
+//eksperisi
+$a1 = $b * $c;
+$a2 = $b + $c;
+// cetak ekspresi
+echo "<p> ini ekspresi -> $a1 = $b * $C <p> ";
+echo "<p> ini ekspresi  -> $a1 = $b * $C <p> ";
+echo "<p> yaitu :" .$a1;
+echo "<p> yaitu :" .$a2;
+echo "<p> by nelfi sahra berutu </p>";
+echo "ini statement 1";
+echo "ini statement 2";
+
+$a = $b + $c;
+?>
