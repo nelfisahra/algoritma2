0057770f0471d45d6978d49e6e729cf9a9388360
nelfisahra <nelfisahrasahra@gmail.com>
2024-08-27 13:55:59 +0700

    membuat html dalam php

---
+++ 3 html dalam php.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<title>Judul Web</title>";
+echo "</head>";
+echo "<body>";
+echo "<p>ini adalah contoh file html didalam tag php</p>";
+echo "<p>by :nelfi sahra berutu</p>";
+echo "</body>";
+echo "</html>";
