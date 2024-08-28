2925f05c83d2e3a44c4cdcf062df92333a45eb2d
nelfisahra <nelfisahrasahra@gmail.com>
2024-08-28 11:38:16 +0700

    membuat penulisan komentar

---
+++ 06 penulisan komentar di php.php
@@ -0,0 +1,18 @@
+<?php
+
+// ini adalah komentar 
+echo "Hello word";
+
+/*
+ini adalah komentar 
+yang lebih dari satu
+baris
+*/
+echo "<br>saya sudah hampir dewasa";
+echo "<br>saya sudah minum teh";
+echo "<br> nelfi";
+
+
+
+?>
+
