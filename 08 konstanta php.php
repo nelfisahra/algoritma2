752619aa1f88671899cf5559e4955597332bb987
nelfisahra <nelfisahrasahra@gmail.com>
2024-08-28 14:00:38 +0700

    membuat konstanta php

---
+++ 08 konstanta php.php
@@ -0,0 +1,11 @@
+<?php
+
+// membuat konstanta dengan fungsi define()
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'nelfi sahra berutu');
+define('DB_PAS', 'R4Hasia');
+define('DB_NAME', 'belajar');
+
+// membuat konstanta dengan kata kunci const
+const API_KEY ="1234";
+?>
