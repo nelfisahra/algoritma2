94b9e159b533f9e1c0e370a8ef268e92683fce22
nelfisahra <nelfisahrasahra@gmail.com>
2024-08-28 10:52:40 +0700

    membuat case sensitif

---
+++ 5 case sensitif.php
@@ -0,0 +1,25 @@
+<?php
+
+$nama ="nelfi sahra berutu";
+$NAMAGURUSAYA = "BAPAK MAHDI";
+$UMUR = 23;
+const INI_KONSTANTA = 123 ;
+$ini_variabel = 23;
+$inijugavariabel = 49 ;
+$inijugavariabel ;
+$tahunlahir =2008;
+
+echo "<p> nama saya adalah $nama";
+echo "<p> nama guru saya adalah $NAMAGURUSAYA";
+ECHO "<p> umur saya adalah $umur";
+echo "<p> saya lahir tahun $umur";
+
+echo
+
+"<p> coba pakai blok program",
+"<p> nama saya adalah $nama",
+"<p> nama guru saya adalah $NAMAGURUSAYA",
+"<p> umur saya $umur tahun";
+"<p> saya lahir tahun $tahunlahir";
+
+?>
